ceb42475698f6c1d9d2e52469e56b8764d0ba5df
Yun.Hu <yunh@dxc.com>
2023-03-25 16:22:54 +0800

    update

---
+++ src/test/java/tech/testnx/cah/quickstart/Tests.java
@@ -0,0 +1,16 @@
+package tech.testnx.cah.quickstart;
+
+import tech.testnx.cah.AbstractTest;
+
+/**
+ * @author Yun
+ *
+ */
+public class Tests extends AbstractTest {
+
+	@Override
+	public String getModuleName() {
+		return "example-module";
+	}
+
+}
